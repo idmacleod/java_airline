5f58929f0efec070f17420b0cbddde6cc168dd00
idmacleod <idmacleod@gmail.com>
2020-06-19 17:23:08 +0100

    Added Passenger tests

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,2 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PassengerTest {
+    private Passenger passenger;
+
+    @Before
+    public void before() {
+        passenger = new Passenger("Iain", 2);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Iain", passenger.getName());
+    }
+
+    @Test
+    public void hasBags() {
+        assertEquals(2, passenger.getBags());
+    }
 }

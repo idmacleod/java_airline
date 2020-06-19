f73ac0a791ce6c58b312d869dcc57823affb0004
idmacleod <idmacleod@gmail.com>
2020-06-19 18:12:55 +0100

    Added Flight tests

--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,2 +1,45 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class FlightTest {
+    private Flight flight;
+    private Plane plane;
+
+    @Before
+    public void before() {
+        plane = new Plane(PlaneType.CESSNA172);
+        flight = new Flight(plane, "IM777", "VCE", "EDI", "06:15");
+    }
+
+    @Test
+    public void flightStartsEmpty() {
+        assertEquals(0, flight.countPassengers());
+    }
+
+    @Test
+    public void hasPlane {
+        assertEquals(PlaneType.CESSNA172, flight.getPlane());
+    }
+
+    @Test
+    public void hasNumber {
+        assertEquals("IM777", flight.getNumber());
+    }
+
+    @Test
+    public void hasDestination {
+        assertEquals("VCE", flight.getDestination());
+    }
+
+    @Test
+    public void hasDepartAirport {
+        assertEquals("EDI", flight.getDepartAirport());
+    }
+
+    @Test
+    public void hasDepartTime {
+        assertEquals("06:15", flight.getDepartTime());
+    }
 }

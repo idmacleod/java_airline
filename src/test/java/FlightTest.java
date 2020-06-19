011cd6b600e15c699355c63b05a2959105e77973
idmacleod <idmacleod@gmail.com>
2020-06-19 18:58:39 +0100

    Added test to ensure cannot book passenger on full flight

--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -78,4 +78,14 @@ public class FlightTest {
         flight.bookPassenger(passenger4);
         assertFalse(flight.seatsAvailable());
     }
+
+    @Test
+    public void cannotBookPassengerIfNoSeats() {
+        flight.bookPassenger(passenger1);
+        flight.bookPassenger(passenger2);
+        flight.bookPassenger(passenger3);
+        flight.bookPassenger(passenger4);
+        flight.bookPassenger(passenger5);
+        assertEquals(4, flight.countPassengers());
+    }
 }

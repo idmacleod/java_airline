f30078294e0200ba1a3b421a31698f8dc85f5092
idmacleod <idmacleod@gmail.com>
2020-06-19 17:43:17 +0100

    Added PlaneType enum

--- src/main/java/PlaneType.java
+++ src/main/java/PlaneType.java
@@ -1,2 +1,21 @@
 public enum PlaneType {
-}
+    BOEING747(400, 16000),
+    AIRBUS220(120, 4000),
+    CESSNA172(4, 60);
+
+    private int capacity;
+    private int totalWeight;
+
+    PlaneType(int capacity, int totalWeight) {
+        this.capacity = capacity;
+        this.totalWeight = totalWeight;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public int getTotalWeight() {
+        return this.totalWeight;
+    }
+}   

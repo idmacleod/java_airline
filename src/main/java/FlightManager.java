e2d969abef05cead17dc2f5d5ce7df78fa7330cc
idmacleod <idmacleod@gmail.com>
2020-06-21 12:50:40 +0100

    Added baggageAllowance() method

--- src/main/java/FlightManager.java
+++ src/main/java/FlightManager.java
@@ -1,2 +1,6 @@
 public class FlightManager {
+    public static double baggageAllowance(Flight flight) {
+        Plane plane = flight.getPlane();
+        return plane.getTotalWeight() / 2 / plane.getCapacity();
+    }
 }

56e008584be23aa68bf7451ba9475f66c9ca4cf5
idmacleod <idmacleod@gmail.com>
2020-06-21 14:12:57 +0100

    Added getBaggageUsed() method

--- src/main/java/FlightManager.java
+++ src/main/java/FlightManager.java
@@ -1,10 +1,18 @@
+import java.util.ArrayList;
+
 public class FlightManager {
     public static double getBaggageAllowance(Flight flight) {
         Plane plane = flight.getPlane();
         return plane.getTotalWeight() / 2 / plane.getCapacity();
     }
 
-//    public static double getBaggageUsed(Flight flight) {
-//
-//    }
+    public static double getBaggageUsed(Flight flight) {
+        ArrayList<Passenger> passengers = flight.getPassengers();
+        double total = 0.00;
+        for (Passenger passenger : passengers) {
+            // Assuming each bag weighs 15kg
+            total += (passenger.getBags() * 15.00);
+        }
+        return total;
+    }
 }

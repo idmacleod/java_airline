f529fe2537fffed7eedeaa6f49e14173cb7ed754
idmacleod <idmacleod@gmail.com>
2020-06-21 13:06:30 +0100

    Tweaked total weights of planes

--- src/main/java/PlaneType.java
+++ src/main/java/PlaneType.java
@@ -1,7 +1,7 @@
 public enum PlaneType {
-    BOEING747(400, 16000.00),
-    AIRBUS220(120, 4000.00),
-    CESSNA172(4, 60.00);
+    BOEING747(400, 20000.00),
+    AIRBUS220(120, 5000.00),
+    CESSNA172(4, 100.00);
 
     private int capacity;
     private double totalWeight;
